a770c20dbe8f989e16651fafe88374eb8ce9f222
upendra <upendra@upendras-MacBook-Air.local>
2023-05-23 01:30:08 +0530

    removed unnecessary things

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,16 @@ import { MDBFooter } from "mdb-react-ui-kit";
 const Footer = () => {
   return (
     <div className={classes.relative}>
-      <MDBFooter sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }} bgColor="light" className="text-center text-lg-left">
+      <MDBFooter
+        sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+        bgColor="light"
+        className="text-center text-lg-left"
+      >
         <div
           className="text-center p-3"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
         >
-          &copy; {new Date().getFullYear()} Copyright{" "}
+          &copy; {new Date().getFullYear()}
         </div>
       </MDBFooter>
     </div>
